f772af029fcde4d52209a17253447c1bd7f06450
c9s <yoan@maicoin.com>
2022-06-07 13:35:28 +0800

    add doccomment for response data

--- step4/get_deposit_address_request.go
+++ step4/get_deposit_address_request.go
@@ -11,6 +11,17 @@ type DepositAddress struct {
 	Url     string `json:"url"`
 }
 
+// GetDepositAddressRequest returns the address of deposit:
+// If network is not send, return with default network of the coin.
+// You can get network and isDefault in networkList in the response of Get /sapi/v1/capital/config/getall (HMAC SHA256).
+//
+// {
+//    "address": "1HPn8Rx2y6nNSfagQBKy27GB99Vbzg89wv",
+//    "coin": "BTC",
+//    "tag": "",
+//    "url": "https://btc.com/1HPn8Rx2y6nNSfagQBKy27GB99Vbzg89wv"
+// }
+//
 //go:generate requestgen -method GET -url "/sapi/v1/capital/deposit/address" -type GetDepositAddressRequest -responseType .DepositAddress
 type GetDepositAddressRequest struct {
 	client requestgen.AuthenticatedAPIClient
--- step4/get_deposit_history_request.go
+++ step4/get_deposit_history_request.go
@@ -23,6 +23,39 @@ type DepositHistory struct {
 	ConfirmTimes  string                     `json:"confirmTimes"`
 }
 
+// GetDepositHistoryRequest returns the following structure:
+// [
+//    {
+//        "amount":"0.00999800",
+//        "coin":"PAXG",
+//        "network":"ETH",
+//        "status":1,
+//        "address":"0x788cabe9236ce061e5a892e1a59395a81fc8d62c",
+//        "addressTag":"",
+//        "txId":"0xaad4654a3234aa6118af9b4b335f5ae81c360b2394721c019b5d1e75328b09f3",
+//        "insertTime":1599621997000,
+//        "transferType":0,
+//        "unlockConfirm":"12/12",  // confirm times for unlocking
+//        "confirmTimes":"12/12"
+//    },
+//    {
+//        "amount":"0.50000000",
+//        "coin":"IOTA",
+//        "network":"IOTA",
+//        "status":1,
+//        "address":"SIZ9VLMHWATXKV99LH99CIGFJFUMLEHGWVZVNNZXRJJVWBPHYWPPBOSDORZ9EQSHCZAMPVAPGFYQAUUV9DROOXJLNW",
+//        "addressTag":"",
+//        "txId":"ESBFVQUTPIWQNJSPXFNHNYHSQNTGKRVKPRABQWTAXCDWOAKDKYWPTVG9BGXNVNKTLEJGESAVXIKIZ9999",
+//        "insertTime":1599620082000,
+//        "transferType":0,
+//        "unlockConfirm":"1/12",
+//        "confirmTimes":"1/1"
+//    }
+// ]
+//
+// Please notice the default startTime and endTime to make sure that time interval is within 0-90 days.
+// If both startTime and endTime are sent, time between startTime and endTime must be less than 90 days.
+//
 //go:generate requestgen -method GET -url "/sapi/v1/capital/deposit/hisrec" -type GetDepositHistoryRequest -responseType []DepositHistory
 type GetDepositHistoryRequest struct {
 	client requestgen.AuthenticatedAPIClient
